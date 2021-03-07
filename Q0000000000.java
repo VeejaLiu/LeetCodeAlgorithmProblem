01c2e76ef830aeb0668a89e5c35a47755426ae0f
veejaLiu <veejaliu@gmail.com>
2021-03-07 23:02:52 +0800

    Add template class.

---
+++ Q0000000000.java
@@ -0,0 +1,15 @@
+package com.veeja.leetCode;
+
+import org.junit.Test;
+
+/**
+ * @Author veeja
+ * 2021/3/7 23:01
+ */
+public class Q0000000000 {
+
+    @Test
+    public void test() {
+
+    }
+}
