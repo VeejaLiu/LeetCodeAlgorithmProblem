19cb9d1b0b00472ea92bcb67b2e4e285fe026eae
veejaLiu <veejaliu@gmail.com>
2021-03-08 10:20:31 +0800

    add Question 1503

---
+++ Q1503.java
@@ -0,0 +1,55 @@
+package com.veeja.leetCode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 《挑战程序设计竞赛》第2版 题 1.6.2
+ * LeetCode Question 1503
+ *
+ * @Author veeja
+ * @date
+ */
+public class Q1503 {
+
+    @Test
+    public void test() {
+
+        int n = 7;
+        int[] left = {};
+        int[] right = {0, 1, 2, 3, 4, 5, 6, 7};
+        System.out.println(getLastMoment(n, left, right));
+
+    }
+
+    public int getLastMoment(int n, int[] left, int[] right) {
+
+        Arrays.sort(left);
+        Arrays.sort(right);
+        if (right.length == 0) {
+            return left[left.length - 1];
+        }
+        if (left.length == 0) {
+            return n - right[0];
+        }
+        if (left.length == 0 && right.length == 0) {
+            return 0;
+        }
+        return Integer.max(left[left.length - 1], n - right[0]);
+    }
+
+    public int getLastMoment2(int n, int[] left, int[] right) {
+
+        int result = 0;
+
+        for (int i = 0; i < left.length; i++) {
+            result = result > left[i] ? result : left[i];
+        }
+        for (int i = 0; i < right.length; i++) {
+            result = result > (n - right[i]) ? result : (n - right[i]);
+        }
+        return result;
+    }
+}
