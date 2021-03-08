c73b060489726b348acddee82b87f9b05384f4ba
veejaLiu <veejaliu@gmail.com>
2021-03-08 12:02:44 +0800

    add Q0509.java

--- jianzhioffer/Q0010_01.java
+++ jianzhioffer/Q0010_01.java
@@ -3,6 +3,31 @@ package com.veeja.leetCode.jianzhioffer;
 import org.junit.Test;
 
 /**
+ * 剑指 Offer 10- I. 斐波那契数列
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+ * <p>
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ * <p>
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 2
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：n = 5
+ * 输出：5
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= n <= 100
+ *
  * @Author veeja
  * 2021/3/8 11:16
  */
@@ -16,15 +41,15 @@ public class Q0010_01 {
 
     public int fib(int n) {
         int result = 0, fib0 = 0, fib1 = 1;
-        if (n == 1 || n == 2) {
+        if (n == 1) {
             return 1;
         }
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             result = (fib0 + fib1) % 1000000007;
             fib0 = fib1;
             fib1 = result;
             System.out.println(result);
         }
-        return fib0;
+        return result;
     }
 }
---
+++ question/Q0509.java
@@ -0,0 +1,72 @@
+package com.veeja.leetCode.question;
+
+import org.junit.Test;
+
+/**
+ * LeetCode Question 509.斐波那契数
+ * <p>
+ * 斐波那契数，通常用F(n) 表示，形成的序列称为 斐波那契数列 。
+ * 该数列由0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0，F(1)= 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给你 n ，请计算 F(n) 。
+ * 示例 1：
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ *
+ * @Author veeja
+ * @date 2021年3月8日11:48:56
+ */
+public class Q0509 {
+
+    @Test
+    public void test() {
+
+        System.out.println("0:" + fib(0));
+        System.out.println("1:" + fib(1));
+        System.out.println("2:" + fib(2));
+        System.out.println("3:" + fib(3));
+        System.out.println("4:" + fib(4));
+        System.out.println("5:" + fib(5));
+        System.out.println("6:" + fib(6));
+        System.out.println("7:" + fib(7));
+        System.out.println("8:" + fib(8));
+        System.out.println("9:" + fib(9));
+        System.out.println("10:" + fib(10));
+        System.out.println("11:" + fib(11));
+        System.out.println("12:" + fib(12));
+        System.out.println("13:" + fib(13));
+        System.out.println("14:" + fib(14));
+        System.out.println("15:" + fib(15));
+        System.out.println("16:" + fib(16));
+        System.out.println("17:" + fib(17));
+        System.out.println("18:" + fib(18));
+        System.out.println("19:" + fib(19));
+        System.out.println("20:" + fib(20));
+        System.out.println("21:" + fib(21));
+        System.out.println("22:" + fib(22));
+        System.out.println("23:" + fib(23));
+        System.out.println("24:" + fib(24));
+        System.out.println("25:" + fib(25));
+        System.out.println("26:" + fib(26));
+        System.out.println("27:" + fib(27));
+        System.out.println("28:" + fib(28));
+        System.out.println("29:" + fib(29));
+        System.out.println("30:" + fib(30));
+    }
+
+    public int fib(int n) {
+        int result = 0, fib0 = 0, fib1 = 1;
+        if (n == 1) {
+            return 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            result = fib0 + fib1;
+            fib0 = fib1;
+            fib1 = result;
+        }
+        return result;
+    }
+
+}
