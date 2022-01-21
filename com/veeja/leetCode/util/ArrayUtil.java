0d4745c389c9718790a08206278eda29dc3f3fb1
VeejaLiu <veejaliu@gmail.com>
2022-01-21 10:09:28 +0800

    Add Array Util class

---
+++ com/veeja/leetCode/util/ArrayUtil.java
@@ -0,0 +1,13 @@
+package com.veeja.leetCode.util;
+
+/**
+ * @author liuweijia
+ */
+public class ArrayUtil {
+    public static void printArray(int[] result) {
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+    }
+}
