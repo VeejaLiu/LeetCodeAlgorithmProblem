83f60ad4b7a7484e3d80a86ed146282eb1b56280
VeejaLiu <veejaliu@gmail.com>
2022-01-26 20:18:17 +0800

    这个题 狗都不看

---
+++ com/veeja/jianzhioffer/Q009.java
@@ -0,0 +1,23 @@
+package com.veeja.jianzhioffer;
+
+public class Q009 {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class CQueue {
+
+    public CQueue() {
+
+    }
+
+    public void appendTail(int value) {
+
+    }
+
+    public int deleteHead() {
+        return 0;
+    }
+}
