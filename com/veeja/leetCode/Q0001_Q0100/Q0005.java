691c4c539da12606fdb8efc04d9cc841362da1c2
VeejaLiu <veejaliu@gmail.com>
2025-03-07 18:47:08 +0800

    优化 LeetCode 5 最长回文子串题目实现，改进动态规划算法及测试输出格式

--- com/veeja/leetCode/Q0001_Q0100/Q0005.java
+++ com/veeja/leetCode/Q0001_Q0100/Q0005.java
@@ -1,8 +1,7 @@
 package com.veeja.leetCode.Q0001_Q0100;
 
+import com.veeja.leetCode.util.ArrayUtil;
 import org.junit.Test;
-
-import java.util.HashSet;
 
 /**
  * 给你一个字符串 s，找到 s 中最长的 回文 子串。
@@ -25,58 +24,69 @@ public class Q0005 {
 
     @Test
     public void test() {
-        System.out.println(longestPalindrome("bababd"));
-        System.out.println(longestPalindrome("cbbd"));
-        System.out.println(longestPalindrome("a"));
-        System.out.println(longestPalindrome("acaca"));
-        System.out.println(longestPalindrome("abba"));
+
+        System.out.println("bababd: " + longestPalindrome("bababd"));
+        System.out.println("cbbd: " + longestPalindrome("cbbd"));
+        System.out.println("a: " + longestPalindrome("a"));
+        System.out.println("aaaaaaa: " + longestPalindrome("aaaaaaa"));
+        System.out.println("acaca: " + longestPalindrome("acaca"));
+        System.out.println("abba: " + longestPalindrome("abba"));
     }
 
     public static String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+//        for (int i = 0; i < n; i++) {
+//            // 初始化，每个字符本身就是一个回文子序列
+//            dp[i][i] = true;
+//        }
+//        ArrayUtil.printArray(dp);
 
-        String result = "";
-        int resultLength = 0;
-        // 应付ABA形式的回文
-        for (int i = 0; i < s.length(); i++) {
-            int temLength = 1;
-            String temResult = "";
-            temResult += s.charAt(i);
-            for (int j = 1; j <= s.length(); j++) {
-                if ((i + j) <= s.length() - 1 && (i - j) >= 0 && s.charAt(i - j) == s.charAt(i + j)) {
-                    temResult = s.charAt(i - j) + temResult + s.charAt(i - j);
-                    temLength += 2;
-                } else {
+        int startIndex = 0;
+        int maxLen = 1;
+
+
+        // L 表示子串的长度
+        for (int L = 1; L <= n; L++) {
+//            System.out.println("L = " + L);
+            // start 表示 子串的起始位置
+            for (int start = 0; start < n; start++) {
+                // end 表示 子串的结束位置
+                int end = start + L - 1;
+                // 如果 end >= n，说明已经越界，结束循环
+                if (end >= n) {
                     break;
                 }
-            }
-            if (temLength > resultLength) {
-                result = temResult;
-                resultLength = temLength;
+
+//                System.out.println("start = " + start);
+//                System.out.println("s.charAt(start) = " + s.charAt(start) + ", s.charAt(end) = " + s.charAt(end));
+
+                if (s.charAt(start) == s.charAt(end)) {
+                    if (end - start <= 1) {
+                        // 如果i j 相邻 或者 i == j，那么 dp[i][j] = true
+                        dp[start][end] = true;
+                    } else {
+                        // 如果i j 不相邻
+                        if (dp[start + 1][end - 1]) {
+                            // 如果 dp[i + 1][j - 1] 也是回文，那么 dp[i][j] = dp[i + 1][j - 1] + 2
+                            dp[start][end] = true;
+                        } else {
+                            // 如果 dp[i + 1][j - 1] 不是回文，那么 dp[i][j] = 0
+                            dp[start][end] = false;
+                        }
+                    }
+                }
+
+                if (dp[start][end] && (end - start + 1 > maxLen)) {
+                    startIndex = start;
+                    maxLen = end - start + 1;
+                }
+
+//                ArrayUtil.printArray(dp);
             }
         }
 
-        // 应付ABBA形式的回文
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) != s.charAt(i + 1)) {
-                continue;
-            }
-            int temLength = 2;
-            String temResult = "";
-            temResult = temResult + s.charAt(i) + s.charAt(i + 1);
-            for (int j = 1; j <= s.length(); j++) {
-                if ((i + 1 + j) <= s.length() - 1 && (i - j) >= 0 && s.charAt(i - j) == s.charAt(i + 1 + j)) {
-                    temResult = s.charAt(i - j) + temResult + s.charAt(i - j);
-                    temLength += 2;
-                } else {
-                    break;
-                }
-            }
-            if (temLength > resultLength) {
-                result = temResult;
-                resultLength = temLength;
-            }
-        }
 
-        return result;
+        return s.substring(startIndex, startIndex + maxLen);
     }
 }
