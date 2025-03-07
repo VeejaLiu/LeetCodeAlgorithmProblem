e0034f14b2072ba09859c6c96b0e8d784444f6ea
VeejaLiu <veejaliu@gmail.com>
2025-03-07 19:14:26 +0800

    新增 ArrayUtil 类中 boolean 数组和二维数组的打印方法，优化输出格式

--- com/veeja/leetCode/util/ArrayUtil.java
+++ com/veeja/leetCode/util/ArrayUtil.java
@@ -5,10 +5,19 @@ package com.veeja.leetCode.util;
  */
 public class ArrayUtil {
     public static void printArray(int[] result) {
+        System.out.print("[ ");
         for (int j : result) {
             System.out.print(j + " ");
         }
-        System.out.println();
+        System.out.println("]");
+    }
+
+    private static void printArray(boolean[] i) {
+        System.out.print("[ ");
+        for (boolean j : i) {
+            System.out.print((j ? "T" : "F") + " ");
+        }
+        System.out.println("]");
     }
 
     public static String toString(int[] result) {
@@ -24,4 +33,11 @@ public class ArrayUtil {
             printArray(i);
         }
     }
+
+    public static void printArray(boolean[][] result) {
+        for (boolean[] i : result) {
+            printArray(i);
+        }
+    }
+
 }
