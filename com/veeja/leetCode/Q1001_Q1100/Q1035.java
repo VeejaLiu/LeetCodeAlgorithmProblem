f7fc110d9e8d5adde210721d0b0b23e36e9d998e
VeejaLiu <veejaliu@gmail.com>
2025-03-10 17:19:16 +0800

    新增 LeetCode 1035 不相交的线题目实现及注释

--- com/veeja/leetCode/Q1001_Q1100/Q1035.java
+++ com/veeja/leetCode/Q1001_Q1100/Q1035.java
@@ -2,9 +2,13 @@ package com.veeja.leetCode.Q1001_Q1100;
 
 
 /**
- * @Author veeja
- * @Email veejaliu@gmail.com
- * 2021/3/6 10:34
+ * 1035. 不相交的线
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ * 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足：
+ * nums1[i] == nums2[j]
+ * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
  */
 public class Q1035 {
     public static void main(String[] args) {
@@ -60,3 +64,16 @@ public class Q1035 {
         System.out.println("----");
     }
 }
+
+/*
+    1  3  5
+ 1  1  1  1
+ 5  1  1  2
+ 3  1  2  2
+
+状态转移 类似于：
+如果最后一位相等， dp[i][j] = dp[i-1][j-1] + 1
+如果最后一位不相等： 则 dp[i][j] = max( dp[i-1][j], dp[i][j-1] )
+
+返回值就是 dp的最后一个元素
+ */
